2a1709507dc19c24b2fb169fbf61bbfffec439df
surya vishakan <suryavishakan050@gmail.com>
2022-02-21 08:52:33 +0530

    initial commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./Navbar.module.css";
+
+const Navbar = () => {
+  return (
+    <nav className={styles.navbar}>
+      <ul>
+        <li className={styles.title}>Finance Tracker</li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/signup">Signup</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
+export default Navbar;
